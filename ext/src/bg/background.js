1d8ec76c3596024f86e55813ff8fefad00689898
Sean Thomas Burke <seanburke@prashan3500w764.cnet.cnwk>
2014-07-01 17:16:55 -0700

    init

---
+++ ext/src/bg/background.js
@@ -0,0 +1,24 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+
+//example of using a message handler from the inject scripts
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	chrome.pageAction.show(sender.tab.id);
+    sendResponse();
+  });
+
+$("body").append($("<div>").attr("id","kbb").load(
+	"http://www.kbb.com/toyota/corolla/1996-toyota-corolla .mod-gradiated-content", 
+	{
+		miles: 100, 
+		type: "toyota"
+	}, 
+	function(){
+		console.log("Sean is Awesome");
+		chrome.extension.sendMessage("Sean", "is Awesome");
+}));
