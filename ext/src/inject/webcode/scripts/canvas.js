11d5573e8e08f3d10d1b5fbc0712a6151681d2a7
Sean Burke <hawaiianchimp@gmail.com>
2014-07-16 00:14:45 -0700

    Easier price variables

--- ext/src/inject/webcode/scripts/canvas.js
+++ ext/src/inject/webcode/scripts/canvas.js
@@ -37,10 +37,11 @@ function dot(context, degrees, radius, w, h){
     context.save();
     context.beginPath();
 
-    var x = radius*Math.cos(toRadians(degrees));
-    var y = radius*Math.sin(toRadians(degrees));
+    var x = radius*Math.cos(toRadians(degrees+180));
+    var y = radius*Math.sin(toRadians(degrees+180));
+    console.log({x:x,y:y, degrees: degrees, radius:radius});
 
-    context.translate(x, y);
+    context.translate(0, 0);
     context.scale(w/2, h/2);
     context.arc(1, 1, 1, 0, 2*Math.PI, false);
     context.closePath();
@@ -84,39 +85,46 @@ function drawCanvas(canvasId, data)
     logo240.src = chrome.extension.getURL('/src/inject/webcode/images/logo240.png');
 
     //// Frames
-    var frame = makeRect(0, 0, 280, 220);
+    var frame = makeRect(10, 20, 240, 200);
+
+    var fairprice = data.data.values.privatepartyfair.price;
+    var goodprice = data.data.values.privatepartygood.price;
+    var verygoodprice = data.data.values.privatepartyverygood.price;
+    var excellentprice = data.data.values.privatepartyexcellent.price;
+    var scaleLow = Math.floor(data.data.scale.scaleLow * .85);
+    var scaleHigh = Math.floor(data.data.scale.scaleHigh);
 
 
     //// Abstracted Attributes
-    var kbbStartAngle = (((data.data.values.privatepartyfair.price-data.data.scale.scaleLow)/(data.data.scale.scaleHigh-data.data.scale.scaleLow))*(360-180))+180;
-    var kbbEndAngle = (((data.data.values.privatepartyexcellent.price-data.data.scale.scaleLow)/(data.data.scale.scaleHigh-data.data.scale.scaleLow))*(360-180))+180;
+    var kbbStartAngle = (((fairprice-scaleLow)/(scaleHigh-scaleLow))*(360-180))+180;
+    var kbbEndAngle = (((excellentprice-scaleLow)/(scaleHigh-scaleLow))*(360-180))+180;
     console.log(data);
-    console.log(data.data.values.privatepartyfair.price);
-    console.log(data.data.scale.scaleLow);
-    console.log(data.data.scale.scaleHigh);
-    console.log(data.data.scale.scaleLow);
-    console.log(data.data.values.privatepartyexcellent.price);
+    console.log(fairprice);
+    console.log(scaleLow);
+    console.log(scaleHigh);
+    console.log(excellentprice);
     console.log(kbbStartAngle);
     console.log(kbbEndAngle);
+
     var redSemiCircleStartAngle = kbbEndAngle;
     var greySemiCircleEndAngle = redSemiCircleStartAngle;
     var greenSemiCircleStartAngle = kbbStartAngle;
     var greenSemiCircleEndAngle = redSemiCircleStartAngle;
     var textContent = 'PRIVATE PARTY RANGE';
-    var minPriceContent = "$" + data.data.scale.scaleLow;
-    var maxPriceContent = '$'+ data.data.scale.scaleHigh;
+    var minPriceContent = "$" + scaleLow;
+    var maxPriceContent = '$'+ scaleHigh;
     var excellentPriceRect = makeRect(frame.x + Math.floor((frame.w - 44) * 0.97881 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.17734 + 0.5), 44, 17);
-    var excellentPriceContent = '$' + data.data.values.privatepartyexcellent.price;
+    var excellentPriceContent = '$' + excellentprice;
     var fairPriceRect = makeRect(frame.x + Math.floor((frame.w - 48) * 0.02586 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.17734 + 0.5), 48, 17);
-    var fairPriceContent = '$'+data.data.values.privatepartyfair.price;
+    var fairPriceContent = '$'+fairprice;
     var goodPriceRect = makeRect(frame.x + Math.floor((frame.w - 48) * 0.30172 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.02956 + 0.5), 48, 17);
-    var goodPriceContent = '$'+data.data.values.privatepartygood.price;
+    var goodPriceContent = '$'+ goodprice;
     var veryGoodPriceRect = makeRect(frame.x + Math.floor((frame.w - 48) * 0.71552 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.02956 + 0.5), 48, 17);
-    var veryGoodPriceContent = '$'+data.data.values.privatepartyverygood.price;
+    var veryGoodPriceContent = '$'+verygoodprice;
 
 
     //// Red Semi Circle Drawing
-    arc(context, frame.x + Math.floor(frame.w * 0.07143 + 0.5), frame.y + Math.floor(frame.h * 0.20682) + 0.5, Math.floor(frame.w * 0.96429 + 0.5) - Math.floor(frame.w * 0.07143 + 0.5), Math.floor(frame.h * 1.34318) - Math.floor(frame.h * 0.20682), redSemiCircleStartAngle, 0, true);
+    arc(context, frame.x + Math.floor(frame.w * 0.07336 + 0.5), frame.y + Math.floor(frame.h * 0.20833) + 0.5, Math.floor(frame.w * 0.96525 + 0.5) - Math.floor(frame.w * 0.07336 + 0.5), Math.floor(frame.h * 1.34559) - Math.floor(frame.h * 0.20833), redSemiCircleStartAngle, 0, true);
     context.save();
     shadow(context);
     context.fillStyle = bad;
@@ -142,7 +150,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Grey Semi Circle Drawing
-    arc(context, frame.x + Math.floor(frame.w * 0.07143 + 0.5), frame.y + Math.floor(frame.h * 0.20682) + 0.5, Math.floor(frame.w * 0.96429 + 0.5) - Math.floor(frame.w * 0.07143 + 0.5), Math.floor(frame.h * 1.34318) - Math.floor(frame.h * 0.20682), 180, greySemiCircleEndAngle, true);
+    arc(context, frame.x + Math.floor(frame.w * 0.07336 + 0.5), frame.y + Math.floor(frame.h * 0.20833) + 0.5, Math.floor(frame.w * 0.96525 + 0.5) - Math.floor(frame.w * 0.07336 + 0.5), Math.floor(frame.h * 1.34559) - Math.floor(frame.h * 0.20833), 180, greySemiCircleEndAngle, true);
     context.save();
     shadow(context);
     context.fillStyle = grey;
@@ -168,7 +176,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Green SemiCircle Drawing
-    arc(context, frame.x + Math.floor(frame.w * 0.00000 + 0.5), frame.y + Math.floor(frame.h * 0.12045) + 0.5, Math.floor(frame.w * 1.03571 + 0.5) - Math.floor(frame.w * 0.00000 + 0.5), Math.floor(frame.h * 1.43864) - Math.floor(frame.h * 0.12045), greenSemiCircleStartAngle, greenSemiCircleEndAngle, true);
+    arc(context, frame.x + Math.floor(frame.w * 0.00000 + 0.5), frame.y + Math.floor(frame.h * 0.12010) + 0.5, Math.floor(frame.w * 1.03475 + 0.5) - Math.floor(frame.w * 0.00000 + 0.5), Math.floor(frame.h * 1.43873) - Math.floor(frame.h * 0.12010), greenSemiCircleStartAngle, greenSemiCircleEndAngle, true);
     context.save();
     shadow(context);
     context.fillStyle = good;
@@ -194,7 +202,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Good Dot Drawing
-    oval(context, frame.x + Math.floor((frame.w - 10) * 0.38333) + 0.5, frame.y + Math.floor((frame.h - 10) * 0.12619) + 0.5, 10, 10);
+    oval(context, frame.x + Math.floor((frame.w - 10) * 0.38353) + 0.5, frame.y + Math.floor((frame.h - 10) * 0.12629) + 0.5, 10, 10);
     context.save();
     shadow(context);
     context.fillStyle = good;
@@ -223,7 +231,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Fair Dot Drawing
-    dot(context, kbbStartAngle, 270, 10, 10);
+    dot(context, greenSemiCircleStartAngle, 260/2, 10, 10);
     context.save();
     shadow(context);
     context.fillStyle = good;
@@ -252,7 +260,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Very Good Dot Drawing
-    oval(context, frame.x + Math.floor((frame.w - 10) * 0.66852) + 0.5, frame.y + Math.floor((frame.h - 10) * 0.12619) + 0.5, 10, 10);
+    oval(context, frame.x + Math.floor((frame.w - 10) * 0.66867) + 0.5, frame.y + Math.floor((frame.h - 10) * 0.12629) + 0.5, 10, 10);
     context.save();
     shadow(context);
     context.fillStyle = good;
@@ -281,7 +289,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Oval 5 Drawing
-    oval(context, frame.x + Math.floor((frame.w - 10) * 0.88704) + 0.5, frame.y + Math.floor((frame.h - 10) * 0.27857) + 0.5, 10, 10);
+    oval(context, frame.x + Math.floor((frame.w - 10) * 0.88554) + 0.5, frame.y + Math.floor((frame.h - 10) * 0.28093) + 0.5, 10, 10);
     context.save();
     shadow(context);
     context.fillStyle = good;
@@ -310,7 +318,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Text Drawing
-    var textRect = makeRect(frame.x + Math.floor((frame.w - 94) * 0.50538 + 0.5), frame.y + Math.floor((frame.h - 10) * 0.30000 + 0.5), 94, 10);
+    var textRect = makeRect(frame.x + Math.floor((frame.w - 94) * 0.50303 + 0.5), frame.y + Math.floor((frame.h - 10) * 0.29897 + 0.5), 94, 10);
     context.fillStyle = whiteColor;
     context.font = '9px Tahoma, Verdana, Segoe, sans-serif';
     context.textAlign = 'center';
@@ -318,7 +326,7 @@ function drawCanvas(canvasId, data)
 
 
     //// kbblogo Drawing
-    var kbblogoRect = makeRect(frame.x + Math.floor((frame.w - 100) * 0.53333 + 0.5), frame.y + Math.floor((frame.h - 100) * 1.00000 + 0.5), 100, 100);
+    var kbblogoRect = makeRect(frame.x + Math.floor((frame.w - 100) * 0.53459 + 0.5), frame.y + Math.floor((frame.h - 100) * 1.00000 + 0.5), 100, 100);
     context.beginPath();
     context.rect(kbblogoRect.x, kbblogoRect.y, kbblogoRect.w, kbblogoRect.h);
     context.save();
@@ -328,7 +336,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Min Price Drawing
-    var minPriceRect = makeRect(frame.x + Math.floor((frame.w - 44) * 0.08475 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.85714 + 0.5), 44, 17);
+    var minPriceRect = makeRect(frame.x + Math.floor((frame.w - 44) * 0.08372 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.85561 + 0.5), 44, 17);
     context.fillStyle = blackColor;
     context.font = '12px Tahoma, Verdana, Segoe, sans-serif';
     context.textAlign = 'left';
@@ -336,7 +344,7 @@ function drawCanvas(canvasId, data)
 
 
     //// Max Price Drawing
-    var maxPriceRect = makeRect(frame.x + Math.floor((frame.w - 60) * 0.95000 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.85714 + 0.5), 60, 17);
+    var maxPriceRect = makeRect(frame.x + Math.floor((frame.w - 60) * 0.94975 + 0.5), frame.y + Math.floor((frame.h - 17) * 0.85561 + 0.5), 60, 17);
     context.fillStyle = blackColor;
     context.font = '12px Tahoma, Verdana, Segoe, sans-serif';
     context.textAlign = 'right';
